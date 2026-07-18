ce6d1c5423b8826b0bd93f4f7381d6e01eb25189
Carlos Eduardo Gonzalez Henriquez <cargonzalez0601@gmail.com>
2026-07-18 15:28:00 -0400

    fix: add kit_emergencia label to QuickDeliveryDialog

--- src/components/QuickDeliveryDialog.tsx
+++ src/components/QuickDeliveryDialog.tsx
@@ -50,6 +50,7 @@ const SUPPLY_LABELS: Record<SupplyItem, string> = {
   carpas: "Carpas",
   silla_ruedas: "Silla de ruedas",
   muletas: "Muletas",
+  kit_emergencia: "Kit de emergencia",
   otros: "Otros",
 };
 
